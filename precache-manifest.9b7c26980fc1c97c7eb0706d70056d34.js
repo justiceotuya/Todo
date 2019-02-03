2176ef83dbbe7d8b9050fc69ba27ad6dac980273
Justice Otuya <justice.otuya@digitalpulse.ng>
2019-02-03 11:45:34 +0100

    Updates

---
+++ precache-manifest.9b7c26980fc1c97c7eb0706d70056d34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53d7f35f28fa5aa54e50",
+    "url": "/Todo/static/css/main.c9477306.chunk.css"
+  },
+  {
+    "revision": "53d7f35f28fa5aa54e50",
+    "url": "/Todo/static/js/main.53d7f35f.chunk.js"
+  },
+  {
+    "revision": "0c193d446f061a4fc5eb",
+    "url": "/Todo/static/js/1.0c193d44.chunk.js"
+  },
+  {
+    "revision": "79583ee1fd0424510277",
+    "url": "/Todo/static/js/runtime~main.79583ee1.js"
+  },
+  {
+    "revision": "b509bd4d2c4c36345f8b645f9926e57f",
+    "url": "/Todo/index.html"
+  }
+];
